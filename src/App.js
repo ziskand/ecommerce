8991cc4e6dccd85dd219e3044765fa0c2c8eee0d
Yigal Ziskand <z0n@pop-os.localdomain>
2020-12-17 13:17:21 +0200

    added redirect to checkout through history

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import CheckoutPage from "./pages/checkout/checkout.component";
 import Header from "./components/header/header.component.jsx";
 import SignInAndSignUpPage from "./pages/sign-in-sign-up/sign-in-sign-up.component.jsx";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
@@ -49,6 +50,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
           <Route
             exact
             path="/signin"
---
+++ src/pages/checkout/checkout.component.jsx
@@ -0,0 +1,5 @@
+import "./checkout.styles.scss";
+
+const CheckoutPage = () => <div>CHECKOUT</div>;
+
+export default CheckoutPage;
