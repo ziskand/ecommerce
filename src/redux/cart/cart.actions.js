8de80fcd904765da1daf9799235aaffd372b23b6
Yigal Ziskand <z0n@pop-os.localdomain>
2020-12-17 16:32:51 +0200

    added increase decrease and remove from checkout list - managed by reducers

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -4,6 +4,11 @@ export const toggleCartHidden = () => ({
   type: CartActionTypes.TOGGLE_CART_HIDDEN,
 });
 
+export const removeItem = (item) => ({
+  type: CartActionTypes.REMOVE_ITEM,
+  payload: item,
+});
+
 export const addItem = (item) => ({
   type: CartActionTypes.ADD_ITEM,
   payload: item,
