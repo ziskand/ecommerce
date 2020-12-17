4588853f21a2aa0c17234e155846d5d6195f65a2
Yigal Ziskand <z0n@pop-os.localdomain>
2020-12-17 14:32:26 +0200

    added checkout flow - page, selector, css

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -7,7 +7,6 @@ import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
 import "./cart-icon.styles.scss";
-import CartItem from "../cart-item/cart-item.component";
 
 const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
