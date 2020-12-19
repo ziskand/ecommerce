1d80181a611b26f1bf5e39cf2231c2f459bbc377
Yigal Ziskand <z0n@pop-os.localdomain>
2020-12-19 13:17:23 +0200

    changed faviconm, modified collection component to load data from reducer

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -6,8 +6,7 @@ import { selectCollection } from "../../redux/shop/shop.selectors";
 
 import "./collection.styles.scss";
 
-const CollectionPage = ({ collection, match }) => {
-  console.log("MATCH", match);
+const CollectionPage = ({ collection }) => {
   const { title, items } = collection;
   return (
     <div className="collection-page">
